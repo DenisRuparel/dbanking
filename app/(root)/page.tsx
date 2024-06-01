052e90fe42aa2f868bac20613e4fa08dad7afa58
Denis <deniskalpeshbhai436@gmail.com>
2024-06-01 13:23:49 +0530

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,61 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Denis', lastName: 'Ruparel', email: 'denisruparel28@gmail.com' };
+  const getTimeBasedGreeting = () => {
+    const currentHour = new Date().getHours();
+    if (currentHour < 12) {
+      return 'Good Morning, ';
+    } else if (currentHour < 18) {
+      return 'Good Afternoon, ';
+    } else {
+      return 'Good Evening, ';
+    }
+  };
+
+  const greetingMessage = getTimeBasedGreeting();
+
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title={`${greetingMessage}`}
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />  
+
+          <TotalBalanceBox 
+            // accounts={accountsData}
+            // totalBanks={accounts?.totalBanks}
+            // totalCurrentBalance={accounts?.totalCurrentBalance}
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        {/* <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        /> */}
+      </div>
+
+      <RightSidebar 
+        // user={loggedIn}
+        // transactions={account?.transactions}
+        // banks={accountsData?.slice(0, 2)}
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+      />
+    </section>
   )
 }
 
