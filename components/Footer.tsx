f83cc63ea82c25bef12a9d0ad0649cbc4cee9f0e
Denis <deniskalpeshbhai436@gmail.com>
2024-06-05 18:41:51 +0530

    modify next config to supress ts during build

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = ({ user, type = 'desktop' }: FooterProps) => {
   }
 
   return (
-    <footer className="footer">
+    <footer className="footer m-4">
       <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
         <p className="text-xl font-bold text-gray-700">
           {user?.firstName[0]}
